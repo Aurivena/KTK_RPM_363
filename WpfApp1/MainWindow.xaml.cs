9517112ebb8d42a7da65533a3e11e9ef56118c60
vladk <vladk@DESKTOP-SFJITF4>
2024-11-09 12:11:10 +0300

    Добавьте файлы проекта.

---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private int currentNum = 0;
+        private int defaultNum = 0;
+
+        private void removeNumber(object sender, MouseButtonEventArgs e)
+        {
+           if (listNumbers.SelectedItem != null) {
+                listNumbers.Items.Remove(listNumbers.SelectedItem);
+            }
+        }
+
+        private void appendNumber(object sender, MouseButtonEventArgs e)
+        {
+            listNumbers.Items.Add(currentNum);
+            currentNum++;
+        }
+
+        private void clearListView(object sender, MouseButtonEventArgs e)
+        {
+            listNumbers.Items.Clear();
+            listNumbers.Items.Add(defaultNum);
+            currentNum = 1;
+        }
+    }
+}
